e23c05fabd52563f4851b82fca1df8dc59317226
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-10-11 14:48:37 +0800

    fix extra space.

--- lib/procon.ex
+++ lib/procon.ex
@@ -1,7 +1,7 @@
 defmodule ProCon do
 
   def start(num_producers, num_consumers) do
-    manager_pid  = Manager.start(num_consumers)
+    manager_pid = Manager.start(num_consumers)
     Enum.each 1..num_producers, fn _ ->
       Producer.start(manager_pid)
     end
